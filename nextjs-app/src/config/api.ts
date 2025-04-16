ad1867e130f332e3bb15f4b4ab5917b4f056e87d
Henrik Jacobsen <henrik.jacobsen2001@gmail.com>
2025-04-16 11:26:52 +0200

    Remove old nextjs-app directory and .DS_Store

--- nextjs-app/src/config/api.ts
+++
@@ -1,7 +0,0 @@
-export const API_CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_API_URL || 'https://asl-translate-backend.onrender.com',
-  ENDPOINTS: {
-    TRANSLATE: '/translate',
-    JUDGE: '/judge'
-  }
-}; 
--- nextjs-app/src/types/react.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-declare module 'react' {
-  interface ReactElement<P = any, T extends string | JSXElementConstructor<any> = string | JSXElementConstructor<any>> {
-    type: T;
-    props: P;
-    key: Key | null;
-  }
-} 
