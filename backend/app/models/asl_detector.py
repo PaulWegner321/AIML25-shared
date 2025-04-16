e7767afbb0eb6ba77e53b788dd4ea5811ce087a0
Henrik Jacobsen <henrik.jacobsen2001@gmail.com>
2025-04-16 12:45:21 +0200

    Updated model to fit weights architecture

--- backend/app/models/asl_detector.py
+++ backend/app/models/asl_detector.py
@@ -12,17 +12,19 @@ class ASLCNN(nn.Module):
         super(ASLCNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 5 * 5, 128)
-        self.fc2 = nn.Linear(128, 26)  # 26 classes for letters A-Z
+        self.fc1 = nn.Linear(512 * 9, 512)
+        self.fc2 = nn.Linear(512, 26)  # 26 classes for letters A-Z
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
-        x = x.view(-1, 64 * 5 * 5)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = x.view(-1, 512 * 9)
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
