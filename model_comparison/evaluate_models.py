6f956543cfd49d7109a25be2f32278e8ac1c21c2
Henrik Jacobsen <henrik.jacobsen2001@gmail.com>
2025-05-13 22:04:05 +0200

    Cleanup

--- model_comparison/evaluate_models.py
+++ model_comparison/evaluate_models.py
@@ -140,13 +140,6 @@ PROMPT_STRATEGIES = [
     "contrastive"
 ]
 
-# Define the transform for preprocessing images (used by CNN)
-cnn_transform = transforms.Compose([
-    transforms.Resize((64, 64)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-])
-
 # --- Helper Functions ---
 def get_watsonx_token(api_key):
     """Gets or refreshes the IBM Cloud IAM token."""
